46fcc62ee5f9c7bce14119fce0d9cc817f5f4f78
Anthony L <laplane_anthony@yahoo.fr>
2026-02-04 15:41:05 +0100

    rename

---
+++ exercise07.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * INSTRUCTIONS:
+ * Create a class named BankAccount with the property balance.
+ * Add a const CURRENCY
+ * Add methods deposit($amount) to deposit money, and withdraw($amount)
+ *  to withdraw money (if the balance is sufficient).
+ * When displaying amount add the currency
+ * Display a message each time an operation is performed (echo).
+ * When it's working, create a child class SavingAccount 
+ *  => we cannot withdraw more than 100
+ */
--- exercise7.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * INSTRUCTIONS:
- * Create a class named BankAccount with the property balance.
- * Add methods deposit($amount) to deposit money, and withdraw($amount)
- * to withdraw money (if the balance is sufficient).
- * Display a message each time an operation is performed.
- */
